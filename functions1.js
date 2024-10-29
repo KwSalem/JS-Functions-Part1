fbb78daf5aa095c046d2c07393b3ddebb126bb3d
KwSalem <167813808+KwSalem@users.noreply.github.com>
2024-10-30 01:41:27 +0300

    Add printName function

---
+++ functions1.js
@@ -0,0 +1,7 @@
+// Task 1: Create a function named `printName` that prints your name on the screen
+function printName() {
+  console.log("Salem Salman"); // ضع اسمك هنا بدلاً من "Your Name Here"
+}
+
+// Call the function to execute it
+printName();
